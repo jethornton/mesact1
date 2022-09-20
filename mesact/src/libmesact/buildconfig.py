4db4da2f0987b91608dffada133385e72e3133ab
John Thornton <dev@gnipsel.com>
2022-09-20 13:53:34 -0500

    start ini changes
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/buildconfig.py
+++ mesact/src/libmesact/buildconfig.py
@@ -3,6 +3,7 @@ import os, traceback, configparser
 from libmesact import settings
 
 from libmesact import checkconfig
+#from libmesact import buildsetup
 from libmesact import buildini
 from libmesact import buildhal
 from libmesact import buildio
@@ -45,6 +46,7 @@ def build(parent):
 		except OSError:
 			parent.machinePTE.appendPlainText(f'OS error\n {traceback.print_exc()}')
 
+	#buildsetup.build(parent)
 	buildini.build(parent)
 	buildhal.build(parent)
 	buildio.build(parent)
